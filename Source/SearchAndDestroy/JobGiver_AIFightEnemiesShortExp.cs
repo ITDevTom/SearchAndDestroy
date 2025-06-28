375455fef5f4d520f048646d996b8faae8d4aacf
Tom Dootson <74529354+ITDevTom@users.noreply.github.com>
2025-06-28 10:03:51 +0100

    Normalize indentation and add .editorconfig

--- Source/SearchAndDestroy/JobGiver_AIFightEnemiesShortExp.cs
+++ Source/SearchAndDestroy/JobGiver_AIFightEnemiesShortExp.cs
@@ -6,18 +6,18 @@ namespace SearchAndDestroy
 {
     class JobGiver_AIFightEnemiesShortExp : JobGiver_AIFightEnemies
     {
-		protected override Job TryGiveJob(Pawn pawn)
+        protected override Job TryGiveJob(Pawn pawn)
         {
             var originalResponse = pawn.playerSettings.hostilityResponse;
             pawn.playerSettings.hostilityResponse = HostilityResponseMode.Attack;
-			var job = base.TryGiveJob(pawn);
+            var job = base.TryGiveJob(pawn);
             if (job != null)
             {
-				job.expiryInterval = 30;
+                job.expiryInterval = 30;
             }
             pawn.playerSettings.hostilityResponse = originalResponse;
             return job;
-		}
+        }
         protected override bool ExtraTargetValidator(Pawn pawn, Thing target)
         {
             if (target is Pawn targetPawn)
